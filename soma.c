fab78d8e68fc09a096d12b7279ad34e2ba7ff9b5
Rayane Pereira <raypereirasilva13@gmail.com>
2022-07-18 20:38:42 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,10 @@
+int main ()
+{
+    int numero1, numero2, resultado;
+    scanf ("%d", &numero1);
+    scanf ("%d", &numero2);
+    resultado = numero1 + numero2;
+    printf("A soma dos dois números é: %d\n", resultado);
+    system("pause");
+    return(0)
+}
